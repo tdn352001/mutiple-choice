fb43aea4cbc5c379ca3bab4853c72921decfda21
Dung Nguyen~ <dung.nguyen@aioz.io>
2024-06-06 09:03:05 +0700

    Update question request

--- src/services/questions/types.ts
+++ src/services/questions/types.ts
@@ -1,3 +1,5 @@
+import { BaseApiQueryParams } from '@/lib/types/query-params'
+
 type Answer = {
   id: number
   answer: string
@@ -10,4 +12,49 @@ type Question = {
   type: string
   image?: string
   answer: Answer[]
+}
+
+export type GetQuestionsQueryParams = BaseApiQueryParams
+
+export type GetQuestionsResponse = {
+  data: {
+    questions: Question[]
+    meta: {
+      current_page: number
+      next_page?: number
+      prev_page?: number
+      total_pages: number
+      total_items: number
+    }
+  }
+}
+
+export type GetQuestionByIdResponse = {
+  data: Question
+}
+
+export type CreateQuestionRequest = {
+  exam_id: string
+  question: string
+  type: string
+  image?: string
+  answer: string[]
+  correct_answer: string[]
+}
+
+export type UpdateQuestionRequest = {
+  exam_id: string
+  question: string
+  type: string
+  image?: string
+}
+
+export type CreateAnswerRequest = {
+  answer: string
+  is_correct: boolean
+}
+
+export type UpdateAnswerRequest = {
+  answer: string
+  is_correct: boolean
 }
