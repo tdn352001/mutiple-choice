ce33a5033727714b2951c19eb639c27efb7add18
tdn352001 <tdn352001@gmail.com>
2024-07-05 23:17:34 +0700

    Prevent open devtools

--- src/components/layout/providers/app-providers.tsx
+++ src/components/layout/providers/app-providers.tsx
@@ -1,18 +1,59 @@
-"use client";
+'use client'
 
-import ThemeProvider from "./theme-provider";
-import QueryProvider from "./query-provider";
-import AuthProvider from "@/components/layout/providers/auth-provider";
-import { PropsWithChildren } from "react";
+import AuthProvider from '@/components/layout/providers/auth-provider'
+import { PropsWithChildren, useEffect } from 'react'
+import QueryProvider from './query-provider'
+import ThemeProvider from './theme-provider'
 
 const AppProviders = ({ children }: PropsWithChildren) => {
+  useEffect(() => {
+    const shouldPreventDevtools = process.env.NEXT_PUBLIC_PREVENT_DEVTOOL ?? true
+    if (!shouldPreventDevtools) return
+
+    const handlePreventContextMenu = (e: MouseEvent) => e.preventDefault()
+
+    const handlePreventShortcut = (e: KeyboardEvent) => {
+      const forbiddenKeys = [
+        { ctrl: true, shift: true, key: 'I' },
+        { ctrl: true, shift: true, key: 'C' },
+        { ctrl: true, key: 'U' },
+      ]
+      if (forbiddenKeys.some((fk) => fk.ctrl === e.ctrlKey && fk.shift === e.shiftKey && fk.key === e.key)) {
+        e.preventDefault()
+      }
+    }
+
+    const handleDetectDevtools = () => {
+      const isDevtoolsOpen =
+        window.outerWidth - window.innerWidth > 160 || window.outerHeight - window.innerHeight > 160
+      document.body.classList.toggle('hidden', isDevtoolsOpen)
+      if (isDevtoolsOpen) window.alert('DevTools is likely open, please close it to continue')
+    }
+
+    window.addEventListener('contextmenu', handlePreventContextMenu)
+    window.addEventListener('keydown', handlePreventShortcut)
+    window.addEventListener('resize', handleDetectDevtools)
+
+    const interval = setInterval(() => {
+      console.clear()
+      console.log('DevTools usage is not allowed.')
+    }, 5000)
+
+    return () => {
+      window.removeEventListener('contextmenu', handlePreventContextMenu)
+      window.removeEventListener('keydown', handlePreventShortcut)
+      window.removeEventListener('resize', handleDetectDevtools)
+      clearInterval(interval)
+    }
+  }, [])
+
   return (
     <ThemeProvider>
       <QueryProvider>
         <AuthProvider>{children}</AuthProvider>
       </QueryProvider>
     </ThemeProvider>
-  );
-};
+  )
+}
 
-export default AppProviders;
+export default AppProviders
